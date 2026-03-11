d1d7360f7dd6cec851cc7d9473ac5c8e8238989f
Joseph Lukudu <josephlukudu22@gmail.com>
2026-03-11 12:18:01 +0300

    added footer content

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-
+import '../App.css'
+import { Link } from 'react-router-dom';
 export default function () {
   return (
-    <div>
-        <h1>Footer goes hear</h1>    
-    </div>
-  )
+    <footer>
+      <div className="footer-container">
+        <div className="logo">
+          <Link to="/" className="logo-l">
+            <img src="../assets/images/book.png" alt="Logo" />
+            <p>LearnFlow</p>
+          </Link>
+          <p>Top learning experiences that create more talent in the world.</p>
+        </div>
+        <div className="footer-socila-links">
+          <div>
+            <h1>Links</h1>
+            <p>About</p>
+            <p>Program</p>
+            <p>Contact us</p>
+            <p>FAQ</p>
+          </div>
+          <div>
+            <h1>Social</h1>
+            <p>Twitter</p>
+            <p>Linkind</p>
+            <p>Facebook</p>
+          </div>
+          <div>
+            <h1>Legal</h1>
+            <p>Tearm</p>
+            <p>Privacy</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }

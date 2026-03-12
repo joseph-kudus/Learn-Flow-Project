c910932549969a3b0b49e1a3a3d24a32c3070e29
Joseph Lukudu <josephlukudu22@gmail.com>
2026-03-12 18:42:20 +0300

    Added login and Register

--- src/loginsignup/Login.jsx
+++ src/loginsignup/Login.jsx
@@ -1,49 +1,69 @@
 import React from 'react'
 import { Link, Notebook } from 'lucide-react'
-import Register from '../loginsignup/Register'
+import "../pages/Page.css";
+
 export default function Login() {
   const email = "";
   const password = "";
   
   return (
-    <div className='w-fully px-3 py-4 boader ader'>
+    <div>
       <div>
         <div>
-          <div>
-            <Notebook className="h-12 w-12 text-indigo-50 md-2" />
-            <h2>Welcome Back</h2>
-            <p>Signin to access your course</p>
-            <form action="#">
-              <div>
-                <label htmlFor="#">
-                  Email:
-                  <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    placeholder="you@example.com"
-                    required
-                  />
-                </label>
+          <div className="loginpage">
+            <div className="Login-logo">
+              <div className="Login-log flex items-center space-x-4 text-indigo-500">
+                <Notebook className="h-12 w-12 text-indigo-50 md-2" />
+                <p>LearnFlow</p>
               </div>
-              <div>
-                <label htmlFor="#">
-                  Password:
-                  <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    placeholder="******"
-                    required
-                  />
-                </label>
-              </div>
-              <button type='submit'>Submit</button>
-              <div >
-                <p className='text-gray-600'>Don't have an account?</p>
-                <Link to="/Register">Register</Link>
-              </div>
-            </form>
+              <p>Welcome!</p>
+              <h2>Sign in to</h2>
+              <p>Lorem Ipsum is simply</p>
+              <form action="#">
+                <div>
+                  <label htmlFor="#">
+                    Username:
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      placeholder="Enter your user name"
+                      required
+                    />
+                  </label>
+                </div>
+                <div>
+                  <label htmlFor="#">
+                    Password:
+                    <input
+                      type="password"
+                      id="password"
+                      value={password}
+                      placeholder="Enter your user Password"
+                      required
+                    />
+                  </label>
+                  <div className="checkbox">
+                    <input type="checkbox" name="checkbox" id="" />
+                    <p>Remember me</p>
+                    <div className="forgot">
+                      <p>Forgot Password?</p>
+                    </div>
+                  </div>
+                </div>
+                <button type="submit">Log</button>
+                <div>
+                  <p>Don't have an account yet?</p>
+                </div>
+                <div>
+                  <Link to="/Register">Register</Link>
+                </div>
+              </form>
+              <div></div>
+            </div>
+          </div>
+          <div className='discat'>
+            <Link to="../asset"></Link>
           </div>
         </div>
       </div>
--- src/loginsignup/Register.jsx
+++ src/loginsignup/Register.jsx
@@ -1,6 +1,57 @@
+import { Link } from "lucide-react";
 function Register() {
     return (
-        <div>Register</div>
-    )
+      <div>
+        <div className="loginpage">
+          <div className="Login-logo flex items-center space-x-4 text-indigo-500">
+            <Notebook className="h-12 w-12 text-indigo-50 md-2" />
+            <p>LearnFlow</p>
+            <p>Welcome!</p>
+            <h2>Sign in to</h2>
+            <p>Lorem Ipsum is simply</p>
+            <form action="#">
+              <div>
+                <label htmlFor="#">
+                  Username:
+                  <input
+                    type="email"
+                    id="email"
+                    value={email}
+                    placeholder="Enter your user name"
+                    required
+                  />
+                </label>
+              </div>
+              <div>
+                <label htmlFor="#">
+                  Password:
+                  <input
+                    type="password"
+                    id="password"
+                    value={password}
+                    placeholder="Enter your user Password"
+                    required
+                  />
+                </label>
+                <div className="checkbox">
+                  <input type="checkbox" name="checkbox" id="" />
+                  <p>Remember me</p>
+                  <div className="forgot">
+                    <p>Forgot Password?</p>
+                  </div>
+                </div>
+              </div>
+              <button type="submit">Log</button>
+              <div>
+                <p>Don't have an account yet?</p>
+              </div>
+              <div>
+                <Link to="/Register">Register</Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    );
 }
 export default Register;

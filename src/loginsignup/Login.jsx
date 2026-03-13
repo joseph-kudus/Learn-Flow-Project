f4f9331bdd4494a03850fa374761a2d03310ead6
Joseph Lukudu <josephlukudu22@gmail.com>
2026-03-13 03:28:55 +0300

    ll

--- src/loginsignup/Login.jsx
+++ src/loginsignup/Login.jsx
@@ -10,50 +10,75 @@ export default function Login() {
   return (
     <div className="login-container">
       <div className="login-logo">
-        <div className="Login-log flex items-center space-x-4 text-indigo-500">
-          <Notebook className="h-12 w-12 text-indigo-50 md-2" />
-          <p>LearnFlow</p>
-        </div>
+        
 
         <div>
+
           <div>
             <Notebook className="h-12 w-12 text-indigo-50 md-2" />
-            <h2>Welcome Back</h2>
-            <p>Signin to access your course</p>
-            <form action="#">
-              <div>
-                <label htmlFor="#">
-                  Email:
-                  <input
-                    type="email"
-                    id="email"
-                    value={email}
-                    placeholder="you@example.com"
-                    required
-                  />
-                </label>
-              </div>
-              <div>
-                <label htmlFor="#">
-                  Password:
-                  <input
-                    type="password"
-                    id="password"
-                    value={password}
-                    placeholder="******"
-                    required
-                  />
-                </label>
-              </div>
-              <button type='submit'>Submit</button>
-              <div >
-                <p className='text-gray-600'>Don't have an account?</p>
-                <Link to="/Register">Register</Link>
-              </div>
-            </form>
+  
+
+
+          <div className="loginpage">
+            <div className="Login-logo">
+              <p>Welcome!</p>
+              <h2>Sign in to</h2>
+              <p>Lorem Ipsum is simply</p>
+              <form action="#">
+                <div className="Login-logo">
+                  <label htmlFor="#">
+                    Username:
+                    <input
+                      type="email"
+                      id="email"
+                      value={email}
+                      placeholder="Enter your user name"
+                      required
+                      className="inp"
+                    />
+                  </label>
+                  <label htmlFor="#">
+                    Password:
+                    <input
+                      type="password"
+                      id="password"
+                      value={password}
+                      placeholder="Enter your user Password"
+                      required
+                      className="inp"
+                    />
+                  </label>
+                </div>
+                <div>
+                  <div>
+                    <div className="checkbox">
+                      <input type="checkbox" name="checkbox" id="" />
+                      <p>Remember me</p>
+                      <div className="forgot">
+                        <p>Forgot Password?</p>
+                      </div>
+                    </div>
+                    <button type="submit" className='inpi'>Login</button>
+                    <div>
+                      <p className="texts-indigo-600">
+                        Don’y have an Account ?{" "}
+                        <Link to="/register" className='reg'>Register</Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </div>
+
           </div>
         </div>
       </div>
-    </div>
+      <div className="team-discu">
+        <h1>Team discu </h1>
+      </div>
+      </div>
+      <div />
+      </div>
+
   );
-}
+};

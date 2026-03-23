3d6e9a75b14d6e6dead5eed6f0a0209616b5e9ab
Joseph <josephlukudu22@gmail.com>
2026-03-23 11:29:36 +0300

    added content to users dashboard

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -28,11 +28,10 @@ import { Outlet } from "react-router-dom";
 import Sidebar from "../Sidebar/Sidebar";
 
 
-
 export default function Dashboard() {
-  const navigate=useNavigate();
-  const {currentUser}=useAuth();
-  
+  const navigate = useNavigate();
+  const { currentUser } = useAuth();
+
   return (
     <div className="dashboard">
       {/*Movile overlay */}
@@ -79,12 +78,119 @@ export default function Dashboard() {
         {/*--Welecome Banner-- */}
         <div className="welcome-banner">
           <h1>Welcome to Learnflow</h1>
-          <h3>Learn at your own pace with lifetime access on mobile and desktop</h3>
+          <h3>
+            Learn at your own pace with lifetime access on mobile and desktop
+          </h3>
           <hr />
-          <h2>Languages courses</h2>
-          <p>Chose from one over many of courses and learn with industry leading expert </p>
+          <p>Let’s continue Learning!</p>
+          <div className="course-conti">
+            <div className="conti">
+              <img src="" alt="img" />
+              <div className="conti-q">
+                <h3>Introduction to CSS language</h3>
+                <p>Learn different programming languages and its usefulness</p>
+              </div>
+            </div>
+            <div className="conti">
+              <img src="" alt="img" />
+              <div className="conti-q">
+                <h3>Introduction to JavaScript language</h3>
+                <p>Learn different programming languages and its usefulness</p>
+              </div>
+            </div>
+          </div>
+          <div className="experts">
+            <h2>Languages courses</h2>
+            <p>
+              Chose from one over many of courses and learn with industry
+              leading expert{" "}
+            </p>
+            <div className="expert-cart">
+              <div className="card-wrap">
+                <img src="" alt="to css" />
+                <div className="expert1">
+                  <div className="expert11">
+                    <h1>Introduction to CSS language</h1>
+                    <p>
+                      Learn the basics of CSS, and its usefulness in the
+                      programming world
+                    </p>
+                    <div className="ato">
+                      <p>
+                        By <strong>ATO</strong>
+                      </p>
+                      <p>
+                        <strong>$29</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card-wrap">
+                <img src="" alt="to css" />
+                <div className="expert1">
+                  <div className="expert11">
+                    <h1>Introduction to Python language</h1>
+                    <p>
+                      Learn the basics of python, and its usefulness in the
+                      programming world
+                    </p>
+                    <div className="ato">
+                      <p>
+                        By <strong>ATO</strong>
+                      </p>
+                      <p>
+                        <strong>$30</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="expert-cart">
+              <div className="card-wrap">
+                <img src="" alt="to css" />
+                <div className="expert1">
+                  <div className="expert11">
+                    <h1>Introduction to Javascript language</h1>
+                    <p>
+                      Learn the basics of Javascript, and its usefulness in the
+                      programming world
+                    </p>
+                    <div className="ato">
+                      <p>
+                        By <strong>ATO</strong>
+                      </p>
+                      <p>
+                        <strong>$50</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="card-wrap">
+                <img src="" alt="to css" />
+                <div className="expert1">
+                  <div className="expert11">
+                    <h1>Introduction to HTML language</h1>
+                    <p>
+                      Learn the basics of HTML, and its usefulness in the
+                      programming world
+                    </p>
+                    <div className="ato">
+                      <p>
+                        By <strong>ATO</strong>
+                      </p>
+                      <p>
+                        <strong>$35</strong>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        
       </section>
       {/*--footer-- */}
       <footer className="footer"></footer>

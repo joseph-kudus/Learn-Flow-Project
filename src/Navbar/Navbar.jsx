0ea4a194e7bc5e4c756718e147330beabed4d81c
Joseph Lukudu <josephlukudu22@gmail.com>
2026-03-31 20:02:47 +0300

    add mobile responsiveness in navbar

--- src/Navbar/Navbar.jsx
+++ src/Navbar/Navbar.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
-import { Notebook } from "lucide-react";
+import { BarChartHorizontal, Notebook } from "lucide-react";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+  useEffect(() => {
+    const resizeHandler = () => {
+      if (window.innerWidth > 768) {
+        setShowMenu(false);
+      }
+    };
+    window.addEventListener("resize", resizeHandler);
+    return () => window.removeEventListener("resize", resizeHandler);
+  }, []);
+
   return (
-    
-      <div className="header-container">
-        <div className="navbar">
+    <div className="header-container">
+      <div className="navbar">
+        <div className="nav-left">
           <Link to="/Landing" className="logo-l">
             <Notebook />
             <p>LearnFlow</p>
@@ -24,39 +35,30 @@ function Navbar() {
             Pricing
           </Link>
         </div>
-  
+        <div className="nav-right">
+          <Link to="/login" className="cta-button">
+            Lets start your learning journey
+          </Link>
+        </div>
+        <button className="menu-toggle" onClick={() => setShowMenu(!showMenu)}>
+          <BarChartHorizontal />
+        </button>
+      </div>
+      <div className={`mobile-menu ${showMenu ? "show" : ""}`}>
+        <Link to="/about" className="navbara">
+          About
+        </Link>
+        <Link to="/features" className="navbara">
+          Features
+        </Link>
+        <Link to="/pricing" className="navbara">
+          Pricing
+        </Link>
         <Link to="/login" className="cta-button">
           Lets start your learning journey
         </Link>
       </div>
-    
-    
-      /* <header class="header-container">
-      <nav class="nav">
-        <div className="navbar">
-          <Link to="/Landing" className="logo-l">
-            <Notebook />
-            <p>LearnFlow</p>
-          </Link>
-        </div>
-        <div className="nav-li">
-          <Link to="/about" className="navbara">
-            About
-          </Link>
-          <Link to="/features" className="navbara">
-            Features
-          </Link>
-          <Link to="/pricing" className="navbara">
-            Pricing
-          </Link>
-        </div>
-        <Link to="/login" className="cta-button">
-          Lets start your learning journey
-        </Link>
-      </nav>
-    </header>
-    */
-    
+    </div>
   );
 }
 

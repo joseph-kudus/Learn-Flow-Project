47a5c3993a3e4a05f77b1b7547557c4d7f7ef516
Joseph Lukudu <josephlukudu22@gmail.com>
2026-05-26 16:34:50 +0300

    added courses component

--- src/dashboard/courses/AllCourse.jsx
+++ src/dashboard/courses/AllCourse.jsx
@@ -1,88 +1,69 @@
 import React from "react";
+import "../../dashboard/courses/course.css";
 import {
-  unsplash,
   Image1,
   Image2,
   Image3,
   Image4,
   Image5,
 } from "../../assets/images/Myimg";
-import "../../dashboard/courses/course.css";
-import { FaArrowLeft } from "react-icons/fa";
 
-function AllCourse() {
-  const course = {
-    author: "ATO productions",
-    level: "Beginner",
-    lessions: 14,
-    price: "",
-  };
-  const handleBuy = () => {
-    console.log("Buying course:", course.lessions);
-    //payment logic here
-  };
+const Courses = () => {
+  const courses = [
+    {
+      img: Image2,
+      title: "Introduction to CSS language",
+      desc: "Learn the basics of CSS, and its usefulness in the programming world",
+      author: "ATO",
+      price: 29,
+    },
+    {
+      img: Image3,
+      title: "Introduction to python language",
+      desc: "Learn the basics of python, and its usefulness in the programming world",
+      auth: "ATO",
+      price: 30,
+    },
+    {
+      img: Image1,
+      title: "Introduction to javascript language",
+    },
+    {
+      img: Image4,
+    },
+  ];
   return (
-    <div className="allcourses-wrapper">
-      <div className="back-to-course">
-        <button>
-          <FaArrowLeft className="arrow" />
-        </button>
-
-        <span>
-          <h1>Back to Courses</h1>
-        </span>
-      </div>
-      <div className="models">
-        <img src={Image2} alt="model-image" />
-      </div>
-
-      <div className="course-conti">
-        <div className="conti-r">
-          <h3>Introduction to CSS language</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur. Tempus ultrices dui
-            vulputate pulvinar risus. Purus lacus tempus mi nibh ligula. Sit in
-            blandit eget id dictum. Dignissim tincidunt pharetra habitant mi nec
-            curabitur et justo. Tristique massa est magna auctor iaculis diam
-            mauris rhoncus. Justo dignissim odio dolor tortor adipiscing. Non
-            volutpat lacinia arcu elit dignissim at. Magna dolor fermentum ac
-            morbi. Vitae curabitur lacinia natoque turpis enim donec
-            pellentesque. Varius convallis sed ullamcorper nisl ligula pharetra.
-            Volutpat cursus et lorem ut risus in metus venenatis duis. At vitae
-            nullam at at ut. Mattis nisl amet tempus turpis donec vel viverra
-            eu. Faucibus convallis faucibus faucibus leo pretium enim euismod.
-            Elit amet volutpat pellentesque tempus dolor faucibus risus at.
-            Venenatis volutpat nam velit metus cursus ut eget tempor risus.
-            Tortor massa magna amet netus adipiscing ultrices nunc facilisi
-            eleifend. Tincidunt sit volutpat quam eu at rhoncus purus diam. Et
-            condimentum donec ut sed risus mattis metus nisl. Libero praesent
-            bibendum mauris mauris turpis.
-          </p>
-        </div>
-        <div className="course-card">
-          <h2>Course details</h2>
-          <div className="course-info">
-            <div className="info-row">
-              <span className="label">Author:</span>
-              <span className="value">{course.author}</span>
+    <div className="course-conti">
+      <div className="conti-q">
+        <h1>Courses</h1>
+        <p>
+          Unlock the world of web development effortlessly with our innovative
+          e-learning courses. Elevate your skills, build a dynamic portfolio,
+          and launch your web development or no-code career with our
+          industry-aligned certifications and dedicated job placement
+          assistance.
+        </p>
+        <div className="expert-cart">
+          {courses.map((course, idx) => (
+            <div className="card-wrap" key={idx}>
+              <img src={course.img} alt={course.title} className="imgr" />
+              <div className="expert1">
+                <div className="expert11">
+                  <h1>{course.title}</h1>
+                  <p>{course.desc}</p>
+                  <hr />
+                  <div className="ato">
+                    <p>By {course.author}</p>
+                    <p>${course.price}</p>
+                  </div>
+                </div>
+              </div>
             </div>
-            <div className="info-row">
-              <span className="label">Level:</span>
-              <span className="value">{course.level}</span>
-            </div>
-            <div className="info-row">
-              <span className="label">Lesson: </span>
-              <span className="value">{course.lessions}</span>
-            </div>
-            <button className="buy-btn" onClick={handleBuy}>
-              Buy Now{course.price}
-            </button>
-          </div>
-          <button className="next-btn">Next Course</button>
+          ))}
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default AllCourse;
+export default Courses;

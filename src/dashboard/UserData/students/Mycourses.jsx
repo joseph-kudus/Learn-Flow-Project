097590765ef33e85d2709f0a9d7c7d650b885191
Joseph Lukudu <josephlukudu22@gmail.com>
2026-07-03 12:10:52 +0300

    fixed the buying button for course in mycourse

--- src/dashboard/UserData/students/Mycourses.jsx
+++ src/dashboard/UserData/students/Mycourses.jsx
@@ -7,115 +7,148 @@ const Mycourses = () => {
   return (
     <div className="myclass">
       <div className="Course-Content-Container">
-        
         <div className="active-course">
           <h3>Active Courses</h3>
           <div className="arrows">
-           <button> <FaArrowLeftLong /></button>
-           <button> <FaArrowRight /></button>
+            <button>
+              {" "}
+              <FaArrowLeftLong />
+            </button>
+            <button>
+              {" "}
+              <FaArrowRight />
+            </button>
           </div>
-          </div>
-          <div className="active-cours">
-          <div className="course-card">
-            <div className="card_container">
-              <img src="#" alt="Imge here" />
-              <div className="intro">
-                <p><strong>Intro to C++</strong></p>
-                <p>CODING</p>
-              </div>
-            </div>
-            <div className="progress-bar"></div>
-            <div className="card_details">
-              <p>12/32 Classes</p>
-            <p>1hr 45 Minutes</p>
-              <div className="resume-btn">
-              <button onClick={() => ("card_details")}>Resume classes</button>
-                <button> <FaArrowRight /></button>
-                </div>
-            </div>
-          </div>
-          <div className="course-card">
-            <div className="card_container">
-              <img src="#" alt="Imge here" />
-              <div className="intro">
-                <p><strong>Intro to JavaScripts</strong></p>
-                <p>CODING</p>
-              </div>
-            </div>
-            <div className="progress-bar"></div>
-            <div className="card_details">
-              <p>12/32 Classes</p>
-            <p>1hr 45 Minutes</p>
-             
-              <div className="resume-btn">
-              <button onClick={() => ("card_details")}>Resume classes</button>
-                <button> <FaArrowRight /></button>
-                </div>
-            </div>
-          </div>
-        </div>
-        <div className="active-course">
-          <h3>Recently Enrolled</h3>
-          <div className="arrows">
-            <button><FaArrowLeftLong /></button>
-            <button><FaArrowRight /></button>
-          </div>
-          
         </div>
         <div className="active-cours">
           <div className="course-card">
             <div className="card_container">
               <img src="#" alt="Imge here" />
               <div className="intro">
-                <p><strong>Ethical Hacking</strong></p>
+                <p>
+                  <strong>Intro to C++</strong>
+                </p>
                 <p>CODING</p>
               </div>
             </div>
-            
+            <div className="progress-bar"></div>
             <div className="card_details">
               <p>12/32 Classes</p>
-            <p>1hr 45 Minutes</p>
-             <p>4.5 ratings</p>
+              <p>1hr 45 Minutes</p>
               <div className="resume-btn">
-              <button onClick={() => ("card_details")}>Resume classes</button>
-                <button> <FaArrowRight /></button>
-                </div>
+                <button onClick={() => "card_details"}>Resume classes</button>
+                <button>
+                  {" "}
+                  <FaArrowRight />
+                </button>
+              </div>
             </div>
           </div>
           <div className="course-card">
             <div className="card_container">
               <img src="#" alt="Imge here" />
               <div className="intro">
-                <p><strong>Intro to Python</strong></p>
+                <p>
+                  <strong>Intro to JavaScripts</strong>
+                </p>
                 <p>CODING</p>
               </div>
             </div>
-            
+            <div className="progress-bar"></div>
             <div className="card_details">
               <p>12/32 Classes</p>
-            <p>1hr 45 Minutes</p>
-             <p>4.5 ratings</p>
+              <p>1hr 45 Minutes</p>
+
               <div className="resume-btn">
-              <button onClick={() => ("card_details")}>Resume classes</button>
-                <button> <FaArrowRight /></button>
-                </div>
+                <button onClick={() => navigate("/learn/7")}>
+                  Resume classes
+                </button>
+                <button>
+                  {" "}
+                  <FaArrowRight />
+                </button>
+              </div>
             </div>
-            </div>
-            
           </div>
+        </div>
+        <div className="active-course">
+          <h3>Recently Enrolled</h3>
+          <div className="arrows">
+            <button>
+              <FaArrowLeftLong />
+            </button>
+            <button>
+              <FaArrowRight />
+            </button>
+          </div>
+        </div>
+        <div className="active-cours">
+          <div className="course-card">
+            <div className="card_container">
+              <img src="#" alt="Imge here" />
+              <div className="intro">
+                <p>
+                  <strong>Ethical Hacking</strong>
+                </p>
+                <p>CODING</p>
+              </div>
+            </div>
+
+            <div className="card_details">
+              <p>12/32 Classes</p>
+              <p>1hr 45 Minutes</p>
+              <p>4.5 ratings</p>
+              <div className="resume-btn">
+                <button onClick={() => "card_details"}>Resume classes</button>
+                <button>
+                  {" "}
+                  <FaArrowRight />
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="course-card">
+            <div className="card_container">
+              <img src="#" alt="Imge here" />
+              <div className="intro">
+                <p>
+                  <strong>Intro to Python</strong>
+                </p>
+                <p>CODING</p>
+              </div>
+            </div>
+
+            <div className="card_details">
+              <p>12/32 Classes</p>
+              <p>1hr 45 Minutes</p>
+              <p>4.5 ratings</p>
+              <div className="resume-btn">
+                <button onClick={() => "card_details"}>Resume classes</button>
+                <button>
+                  {" "}
+                  <FaArrowRight />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="active-course-activ">
-      <div className="active-course-active">
-        <h3>Recent Activities</h3>
-        <div className="arrows">
-          <button><IoIosMore /></button>
-        </div>
+        <div className="active-course-active">
+          <h3>Recent Activities</h3>
+          <div className="arrows">
+            <button>
+              <IoIosMore />
+            </button>
+          </div>
         </div>
         <div className="actives-wrapper">
           <div className="todays_activies">
             <p>Today, 17 Oct 2024</p>
             <div className="lesson_schedule">
-              <p><strong>13:00</strong></p>
+              <p>
+                <strong>13:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
@@ -123,7 +156,9 @@ const Mycourses = () => {
               <p>Graded 5/5</p>
             </div>
             <div className="lesson_schedule">
-              <p><strong>14:00</strong></p>
+              <p>
+                <strong>14:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
@@ -131,12 +166,13 @@ const Mycourses = () => {
               <button>Upcoming</button>
             </div>
           </div>
-          
-           
-            <div className="todays_activies">
+
+          <div className="todays_activies">
             <p>Yesterday, 16 Oct 2024</p>
             <div className="lesson_schedule">
-              <p><strong>13:00</strong></p>
+              <p>
+                <strong>13:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
@@ -144,7 +180,9 @@ const Mycourses = () => {
               <button>Submitted</button>
             </div>
             <div className="lesson_schedule">
-            <p><strong>14:00</strong></p>
+              <p>
+                <strong>14:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
@@ -152,13 +190,13 @@ const Mycourses = () => {
               <button>Missed</button>
             </div>
           </div>
-          
+
           <div className="todays_activies">
-           
-           
             <p>Thursday, 15 Oct 2024</p>
             <div className="lesson_schedule">
-            <p><strong>13:00</strong></p>
+              <p>
+                <strong>13:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
@@ -166,19 +204,18 @@ const Mycourses = () => {
               <button>Missed</button>
             </div>
             <div className="lesson_schedule">
-            <p><strong>14:00</strong></p>
+              <p>
+                <strong>14:00</strong>
+              </p>
               <div className="classes_view">
                 <p>HTML 101</p>
                 <p>Coursework</p>
               </div>
-           <button>Attended</button>
-          </div>
-            
+              <button>Attended</button>
+            </div>
           </div>
         </div>
-        
       </div>
-      
     </div>
   );
 };

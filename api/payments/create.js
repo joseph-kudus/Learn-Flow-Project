bd78880a96dd9ee8379c754d2eecb10cd473be96
Joseph <josephlukudu22@gmail.com>
2026-09-22 00:17:16 +0200

    test: confirm flutterwave v4 api deployment

--- api/payments/create.js
+++ api/payments/create.js
@@ -5,6 +5,10 @@ export default async function handler(req, res) {
       message: "Method not allowed",
     });
   }
+  return res.status(200).json({
+    success: true,
+    version: "flutterwave-v4-test",
+  });
 
   try {
     const {
